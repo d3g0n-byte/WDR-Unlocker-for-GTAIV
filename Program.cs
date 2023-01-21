6cd87df16a69c50659990f9d87b76766eb9fed80
d3g0n-byte <108334730+d3g0n-byte@users.noreply.github.com>
2023-01-22 00:18:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Converter.Utils;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using System;
+using static ICSharpCode.SharpZipLib.Zip.ExtendedUnixData;
+
+namespace General
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1) return;
+			for (int a = 0; a < args.Length; a++)
+			{
+				string fileName = args[a];
+				using (EndianBinaryReader br = new EndianBinaryReader(File.OpenRead(fileName)))
+				{
+					uint magic = br.ReadUInt32();
+					int version = br.ReadInt32();
+					if (version != 110 || magic != 88298322) continue;
+					int flags = br.ReadInt32();
+					uint CPUSize = ((uint)flags & 0x7FF) << (((flags >> 11) & 0xF) + 8); //расчет размера System Memory Segment
+					uint GPUSize = (((uint)flags >> 15) & 0x7FF) << (((flags >> 26) & 0xF) + 8); //расчет размера Graphics Memory Segment
+
+					int dLen = (int)(GPUSize + CPUSize);
+					byte[] origBuffer = br.ReadBytes(Convert.ToInt32(br.BaseStream.Length - br.BaseStream.Position));
+					byte[] decompBuffer = DecompressDeflate(origBuffer, dLen, noHeader: false);
+
+					WDRUnlocker.WDR.Unlock(ref decompBuffer);
+
+					byte[] array = Compress(decompBuffer, 9, noHeader: false);
+					br.Close();
+					//File.Delete(fileName);
+					BinaryWriter dataOut = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+					uint val = 88298322u;
+					int resVersion = 110;
+					dataOut.Write(val);
+					dataOut.Write(resVersion);
+					dataOut.Write(flags);
+					dataOut.Write(array);
+					dataOut.Close();
+
+
+				}
+
+			}
+		}
+
+		public static byte[] DecompressDeflate(byte[] data, int decompSize, bool noHeader = true)
+		{
+			byte[] array = new byte[decompSize];
+			Inflater inflater = new Inflater(noHeader);
+			inflater.SetInput(data);
+			inflater.Inflate(array);
+			return array;
+		}
+		public static byte[] Compress(byte[] input, int level, bool noHeader = true)
+		{
+			byte[] array = new byte[input.Length];
+			Deflater deflater = new Deflater(level, noHeader);
+			byte[] array2;
+			try
+			{
+				deflater.SetInput(input, 0, input.Length);
+				deflater.Finish();
+				array2 = new byte[deflater.Deflate(array)];
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			Array.Copy(array, 0, array2, 0, array2.Length);
+			return array2;
+		}
+
+	}
+}
---
+++ WDR.cs
@@ -0,0 +1,74 @@
+﻿using Converter.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WDRUnlocker
+{
+	internal class WDR
+	{
+		public static byte[] WriteToByteArray(ref byte[] array, uint pos, uint value)
+		{
+			byte[] array2 = BitConverter.GetBytes(value);
+			for (int a = 0; a < array2.Length; a++)
+			{
+				array[pos + a] = array2[a];
+			}
+			//pos += (uint)array2.Length;
+			return array;
+		}
+		public struct Collection
+		{
+			public uint m_pList;
+			public ushort m_wCount;
+			public ushort m_wSize;
+		}
+
+		public static void Unlock(ref byte[] wdr)
+		{
+			byte[] newWDR = new byte[wdr.Length];
+			Buffer.BlockCopy(wdr, 0, newWDR, 0, wdr.Length);
+			MemoryStream ms = new MemoryStream(wdr);
+			EndianBinaryReader br = new EndianBinaryReader(ms);
+			//if (endian) br.Endianness = Endian.BigEndian;// 0 - lit, 1 - big
+			br.Position = 0x40;
+			uint[] pModelCollection = new uint[4];
+			for (int a = 0; a < 4; a++) pModelCollection[a] = br.ReadOffset();
+			br.Position = 0x60;
+			int[] count = new int[4];
+			for (int a = 0; a < 4; a++) count[a] = br.ReadInt32();
+			for (int a = 0; a < 4; a++)
+			{
+				if (count[a] < 1) continue;
+				br.Position = pModelCollection[a];
+				Collection cModelCollection = br.ReadCollections();
+				br.Position = cModelCollection.m_pList;
+				uint[] pModel = new uint[cModelCollection.m_wCount];
+				for (int b = 0; b < cModelCollection.m_wCount; b++)pModel[b] = br.ReadOffset();
+				for (int b = 0; b < cModelCollection.m_wCount; b++)
+				{
+					br.Position = pModel[b];
+					br.Position +=4;
+					Collection cGeometry = br.ReadCollections();
+					br.Position = cGeometry.m_pList;
+					uint[] pGeometry = new uint[cGeometry.m_wCount];
+					for (int c = 0; c < cGeometry.m_wCount; c++) pGeometry[c] = br.ReadOffset();
+					for (int c = 0; c < cGeometry.m_wCount; c++)
+					{
+						br.Position = pGeometry[c];
+						br.Position += 0x2c;
+						uint tmp1 = br.ReadUInt32();
+						WriteToByteArray(ref newWDR, (uint)br.Position, tmp1 / 3);
+					}
+				}
+			}
+			ms.Close();
+			br.Close();
+			Buffer.BlockCopy(newWDR, 0, wdr, 0, wdr.Length);
+
+		}
+	}
+}
